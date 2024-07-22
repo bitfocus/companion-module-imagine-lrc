6400d73e984ddb5562c188354b4f813a84f80e08
Ross Lindsay <ross@rosslindsay.com>
2024-07-22 00:31:53 -0400

    Style

--- src/actions.js
+++ src/actions.js
@@ -1,77 +1,77 @@
 module.exports = function (self) {
-    self.setActionDefinitions({
-        sample_action: {
-            name: 'My First Action',
-            options: [
-                {
-                    id: 'num',
-                    type: 'number',
-                    label: 'Test',
-                    default: 5,
-                    min: 0,
-                    max: 100,
-                },
-            ],
-            callback: async (event) => {
-                console.log('Hello world!', event.options.num)
-            },
-        },
-        xpoint_take: {
-            name: "Crosspoint Take",
-            description: "Sends a XPOINT_TAKE command to the router with the specified options",
-            options: [
-                {
-                    type: 'dropdown',
-                    label: 'Source',
-                    id: 'source',
-                    tooltip: 'Specify a source by name (e.g. "SAT 1") or number (e.g. 6)',
-                    minChoicesForSearch: 0,
-                    allowCustom: true,
-                    regex: '/^[^~\\{},]+$/',
-                    choices: self.state.sources,
-                    default: undefined,
-                },
-                {
-                    type: 'dropdown',
-                    label: 'Source Channel (Level)',
-                    id: 'source_level',
-                    tooltip: 'Specify a source level (e.g. 1, "Level 1")',
-                    minChoicesForSearch: 0,
-                    allowCustom: true,
-                    regex: '/^[^~\\{},]+$/',
-                    choices: self.state.channels,
-                    default: undefined,
-                },
-                {
-                    type: 'multidropdown',
-                    label: 'Destination',
-                    id: 'destination',
-                    tooltip:
-                        'Specify a destination by name (e.g. "MON 6") or number (e.g. 1). ' +
-                        'Multiple destinations may be selected/entered if desired.' +
-                        '*WARNING* An empty destination resolves to all logical destinations.',
-                    minChoicesForSearch: 0,
-                    allowCustom: true,
-                    multiple: true,
-                    regex: '/^[^~\\{},]+$/',
-                    choices: self.state.destinations,
-                    default: undefined,
-                },
-                {
-                    type: 'dropdown',
-                    label: 'Destination Channel (Level)',
-                    id: 'destination_level',
-                    tooltip: 'Specify a destination level (e.g. 1, "Level 1")',
-                    minChoicesForSearch: 0,
-                    allowCustom: true,
-                    regex: '/^[^~\\{},]+$/',
-                    choices: self.state.channels,
-                    default: undefined,
-                },
-            ],
-            callback: async (event) => {
-                console.log('XPOINT_TAKE', event.options.num)
-            }
-        }
-    })
+	self.setActionDefinitions({
+		sample_action: {
+			name: 'My First Action',
+			options: [
+				{
+					id: 'num',
+					type: 'number',
+					label: 'Test',
+					default: 5,
+					min: 0,
+					max: 100,
+				},
+			],
+			callback: async (event) => {
+				console.log('Hello world!', event.options.num)
+			},
+		},
+		xpoint_take: {
+			name: 'Crosspoint Take',
+			description: 'Sends a XPOINT_TAKE command to the router with the specified options',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Source',
+					id: 'source',
+					tooltip: 'Specify a source by name (e.g. "SAT 1") or number (e.g. 6)',
+					minChoicesForSearch: 0,
+					allowCustom: true,
+					regex: '/^[^~\\{},]+$/',
+					choices: self.state.sources,
+					default: undefined,
+				},
+				{
+					type: 'dropdown',
+					label: 'Source Channel (Level)',
+					id: 'source_level',
+					tooltip: 'Specify a source level (e.g. 1, "Level 1")',
+					minChoicesForSearch: 0,
+					allowCustom: true,
+					regex: '/^[^~\\{},]+$/',
+					choices: self.state.channels,
+					default: undefined,
+				},
+				{
+					type: 'multidropdown',
+					label: 'Destination',
+					id: 'destination',
+					tooltip:
+						'Specify a destination by name (e.g. "MON 6") or number (e.g. 1). ' +
+						'Multiple destinations may be selected/entered if desired.' +
+						'*WARNING* An empty destination resolves to all logical destinations.',
+					minChoicesForSearch: 0,
+					allowCustom: true,
+					multiple: true,
+					regex: '/^[^~\\{},]+$/',
+					choices: self.state.destinations,
+					default: undefined,
+				},
+				{
+					type: 'dropdown',
+					label: 'Destination Channel (Level)',
+					id: 'destination_level',
+					tooltip: 'Specify a destination level (e.g. 1, "Level 1")',
+					minChoicesForSearch: 0,
+					allowCustom: true,
+					regex: '/^[^~\\{},]+$/',
+					choices: self.state.channels,
+					default: undefined,
+				},
+			],
+			callback: async (event) => {
+				console.log('XPOINT_TAKE', event.options.num)
+			},
+		},
+	})
 }
--- src/main.js
+++ src/main.js
@@ -7,56 +7,56 @@ const GetConfigFields = require('./config')
 const utils = require('./utils')
 
 class ImagineLRCInstance extends InstanceBase {
-    constructor(internal) {
-        super(internal)
-        let self = this
+	constructor(internal) {
+		super(internal)
+		let self = this
 
-        // Assign the methods from the listed files to this class
-        Object.assign(self, {
-            ...GetConfigFields,
-            ...utils
-        })
+		// Assign the methods from the listed files to this class
+		Object.assign(self, {
+			...GetConfigFields,
+			...utils,
+		})
 
-        this.state = {
-            'protocol_version': 0,
-            'sources': [],
-            'sources_count': 0,
-            'destinations': [],
-            'destinations_count': 0,
-            'channels': [],
-            'salvos': []
-        }
-    }
+		this.state = {
+			protocol_version: 0,
+			sources: [],
+			sources_count: 0,
+			destinations: [],
+			destinations_count: 0,
+			channels: [],
+			salvos: [],
+		}
+	}
 
-    async init(config) {
-        this.config = config
+	async init(config) {
+		this.config = config
 
-        this.initConnection()
-        this.updateActions()
-        this.updateFeedbacks()
-        this.updateVariableDefinitions()
-    }
-    // When module gets deleted
-    async destroy() {
-        this.log('debug', 'destroy')
-    }
+		this.initConnection()
+		this.updateActions()
+		this.updateFeedbacks()
+		this.updateVariableDefinitions()
+	}
+	// When module gets deleted
+	async destroy() {
+		this.log('debug', 'destroy')
+	}
 
-    async configUpdated(config) {
-        this.config = config
-        this.initConnection()
-    }
+	async configUpdated(config) {
+		this.config = config
+		this.initConnection()
+	}
 
-    updateActions() {
-        UpdateActions(this)
-    }
+	updateActions() {
+		UpdateActions(this)
+	}
 
-    updateFeedbacks() {
-        UpdateFeedbacks(this)
-    }
+	updateFeedbacks() {
+		UpdateFeedbacks(this)
+	}
 
-    updateVariableDefinitions() {
-        UpdateVariableDefinitions(this)
-    }
+	updateVariableDefinitions() {
+		UpdateVariableDefinitions(this)
+	}
 }
 
 runEntrypoint(ImagineLRCInstance, UpgradeScripts)
