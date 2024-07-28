199af84d20d29774f1fbffeb1221fb436363cf7a
Ross Lindsay <ross@rosslindsay.com>
2024-07-27 21:55:11 -0400

    No variable creation implementation at this time

--- src/variables.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (self) {
-	self.setVariableDefinitions([
-		{ variableId: 'variable1', name: 'My first variable' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
-	])
-}
